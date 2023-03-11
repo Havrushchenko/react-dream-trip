690a8ae32c9413eb9765fd0962721cfb7f8d7d6f
Havrushchenko <glitteringpuzzlemusic@gmail.com>
2023-03-11 15:14:29 +0200

    created Flightout model

---
+++ server/models/Flightout.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require('mongoose');
+
+const flightoutSchema = new Schema(
+    {
+        flight_number: {
+            type: String,
+            required: true,
+        },
+        departure_city: {
+            type: String,
+            required: true,
+        },
+        destination_city: {
+            type: String,
+            required: true,
+        },
+        departure_time: {
+            type: String,
+            required: true,
+        },
+        departure_day: {
+            type: String,
+            required: true,
+        },
+        arrival_time: {
+            type: String,
+            required: true,
+        },
+        departure_airport: {
+            type: String,
+            required: true,
+        },
+        destination_airport: {
+            type: String,
+            required: true,
+        },
+        price: {
+            type: String,
+            required: true,
+        },
+    }
+);
+
+const Flightout = model('Flightout', flightoutSchema);
+
+module.exports = Flightout;
--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,4 @@
 const User = require('./User');
+const Flightout = require('./Flightout');
 
-module.exports = { User };
+module.exports = { User, Flightout };
