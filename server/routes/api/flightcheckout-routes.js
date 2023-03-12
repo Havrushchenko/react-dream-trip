bd08c4370a1a7234771f620eaeadd9f4f8431c13
Havrushchenko <glitteringpuzzlemusic@gmail.com>
2023-03-12 18:14:34 +0200

    merged with main

--- server/routes/api/flightcheckout-routes.js
+++ server/routes/api/flightcheckout-routes.js
@@ -1,12 +1,11 @@
 const router = require('express').Router();
 const {
-    getFlightsbyId,
-    updateFlight,
-    deleteFlight
-  } = require('../../controllers/flightcheckout-routes');
-  
-// api/user/flightcheckout/id
-  router.route('/:userid/flights/').get(getFlightsbyId);
-  router.route('/:userid/flights/:flightid').post(updateFlight).delete(deleteFlight);
-  
-  module.exports = router;
+  getFlightsbyId,
+  updateFlight,
+  deleteFlight
+} = require('../../controllers/flightcheckout-routes');
+
+router.route('/:userid/flights/').get(getFlightsbyId);
+router.route('/:userid/flights/:flightid').post(updateFlight).delete(deleteFlight);
+
+module.exports = router;
--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,17 +1,12 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
-<<<<<<< HEAD
-const flightoutRoutes = require('./flightout-routes')
-
-router.use('/users', userRoutes);
-router.use('/flightouts', flightoutRoutes);
-=======
+const flightoutRoutes = require('./flightout-routes');
 const flightCheckoutRoutes = require('./flightcheckout-routes');
 const passengerRoutes = require('./passenger-routes');
 
 router.use('/users', userRoutes);
+router.use('/flightouts', flightoutRoutes);
 router.user('/users/flights', flightCheckoutRoutes);
 router.user('/users/flights/passengers', passengerRoutes);
->>>>>>> 7f0f8a07de8f950b4122db4e6599dcf1d6e3cb1a
 
 module.exports = router;
