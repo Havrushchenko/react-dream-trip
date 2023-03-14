9e2cb444c003636b07e5e58b1843906221530308
miklywek <miklywek@gmail.com>
2023-03-15 00:33:55 +0200

    rolled back to working version

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -5,11 +5,6 @@ const messageRoutes = require('./message-routes');
 
 router.use('/users', userRoutes);
 router.use('/flightouts', flightoutRoutes);
-<<<<<<< HEAD
-router.use('/flights', flightCheckoutRoutes);
-router.use('/flights/passengers', passengerRoutes);
-=======
 router.use('/messages', messageRoutes);
->>>>>>> 2f8379d26be05a1233728af9ddca8a23f31618e1
 
 module.exports = router;
