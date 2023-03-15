6dda807a069c1506fdb34a53fab6acdc161adce5
Havrushchenko <glitteringpuzzlemusic@gmail.com>
2023-03-15 16:27:26 +0200

    created stripe session

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes');
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -18,8 +17,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
   
-
-
 db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
