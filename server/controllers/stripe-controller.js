3b8cb07d663751ec870bf375dcb32f253ada5b65
Havrushchenko <glitteringpuzzlemusic@gmail.com>
2023-03-15 17:02:03 +0200

    added stripe test key

--- server/controllers/stripe-controller.js
+++ server/controllers/stripe-controller.js
@@ -4,7 +4,7 @@ const YOUR_DOMAIN = 'http://localhost:3001';
 module.exports = {
     async createCheckoutSession(req, res) {
         const session = await stripe.checkout.sessions.create({
-            customer_email: 'customer@example.com',
+            customer_email: 'havrushchenko@gmail.com',
             submit_type: 'donate',
             billing_address_collection: 'auto',
             shipping_address_collection: {
@@ -20,7 +20,6 @@ module.exports = {
             success_url: `${YOUR_DOMAIN}?success=true`,
             cancel_url: `${YOUR_DOMAIN}?canceled=true`,
         });
-
         res.send({ url: session.url });
     }
 };
